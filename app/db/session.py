624e5a5409157f61b8f4dee7ce3d7654ca227c00
Tebas25 <andres.almeida.yanez@udla.edu.ec>
2026-08-31 01:00:01 -0500

    feat(setup-db-models-schemas-): Fix Mongodb client and loop service

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,7 @@ from app.core.config import settings
 from pymongo import AsyncMongoClient
 
 client = AsyncMongoClient(settings.CONNECTION_STRING)
-db = client.dbAnalisisDesarrollo
+db = client.dbAnaliticaDesarrollo
 
 
 def get_databse():
